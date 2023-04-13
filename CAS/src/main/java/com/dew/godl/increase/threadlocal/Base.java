1e0bcf03dbef946893bc9eeefd2575df991f48c2
dew0209 <ly020922@aliyun.com>
2023-04-13 20:51:37 +0800

    [计划][cas][0413][ThreadLocal入门]

---
+++ CAS/src/main/java/com/dew/godl/increase/threadlocal/Base.java
@@ -0,0 +1,11 @@
+package com.dew.godl.increase.threadlocal;
+
+/**
+ * 	ThreadLocal提供线程局部变量，这些局部变量与正常的变量不同，因为每一个线程在访问ThreadLocal实例的时候（通过其get或set方法）都有自己的，独立初始化的变量副本。
+ * 	ThreadLocal实例通常是类中的私有静态字段，使用它的目的是希望将状态（例如：用户id或事务id）与线程关联起来
+ */
+public class Base {
+	public static void main(String[] args) {
+
+	}
+}
