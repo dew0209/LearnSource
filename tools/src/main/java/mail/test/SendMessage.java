b6656383227dd10a4787b518b52a237e6a7fe70e
Guitarist <89758002+dew0209@users.noreply.github.com>
2025-05-21 15:05:46 +0800

    [计划][技术][0521][提交本地代码]

---
+++ tools/src/main/java/mail/test/SendMessage.java
@@ -0,0 +1,66 @@
+package mail.test;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.SimpleEmail;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Properties;
+
+/**
+ * Copyright (c) by [安徽航天信息]
+ *
+ * @author LvLu
+ * @className SendMessage
+ * @date 2024-08-09 9:24
+ * @description
+ */
+
+@FunctionalInterface
+interface InvokeBusinessFun<T> {
+
+	/**
+	 * @title invokeBusiness
+	 * @description 执行业务
+	 * @author chenhai
+	 * @date 2023-08-23 15:14
+	 * @param t 传入参数
+	 * @return: R 返回结果
+	 */
+	String invokeBusiness(T t);
+
+}
+
+
+public class SendMessage {
+	public static void main(String[] args) throws Exception {
+		try {
+			String test = test(1, res -> {
+				int i = 1 / 0;
+				return null;
+			});
+			System.out.println(test);
+		}catch (Exception e){
+			System.out.println("111111");
+		}
+	}
+
+	public static String test(int a,InvokeBusinessFun<InterFaceTest> v){
+		String test = "11";
+		InterFaceTest test1 = new InterFaceTest();
+		v.invokeBusiness(test1);
+		return "111";
+	}
+}
+
+class InterFaceTest {
+	String aa21;
+}
